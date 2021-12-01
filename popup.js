3cacecc4bb52852841e91a295a78984bacf61904
VictorGNetto <victornetto@discente.ufg.br>
2021-12-01 17:24:19 -0300

    versão 1.0 concluída

--- popup.js
+++ popup.js
@@ -1,16 +1,25 @@
-const pasteButton = document.getElementById("pasteButton");
-const textarea = document.getElementById("abc-textarea");
+const textarea = document.getElementById("textarea");
+const okButton = document.getElementById("ok-button");
+const cancelButton = document.getElementById("cancel-button");
 
 textarea.addEventListener("blur", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   let textareaValue = textarea.value;
-  chrome.storage.sync.set({ textareaValue });
+  chrome.storage.local.set({ textareaValue });
+});
+
+okButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: pasteTextareaContent,
   });
+
+  window.close();
+});
+
+cancelButton.addEventListener("click", () => {
+  window.close();
 });
 
 function pasteTextareaContent() {
@@ -18,7 +27,7 @@ function pasteTextareaContent() {
   const iframeContent = iframe.contentDocument || iframe.contentWindow.document;
   const iframeBody = iframeContent.querySelector("body");
 
-  chrome.storage.sync.get("textareaValue", ({ textareaValue }) => {
+  chrome.storage.local.get("textareaValue", ({ textareaValue }) => {
     iframeBody.innerHTML = textareaValue;
   });
 }
